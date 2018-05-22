cf9c21d4fe1c4bc2554bdcf1595808f423b10c8c
Tim Miller <timmil@cisco.com>
2018-05-22 17:35:22 -0400

    Add required license header.

--- nxapi_cli/step-03/connection.py
+++ nxapi_cli/step-03/connection.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Copyright (c) 2018 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.0 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+
+"""
 
 # http://python-future.org/quickstart.html
 from __future__ import absolute_import, division, print_function
+
+__author__ = "Timothy E Miller, PhD <timmil@cisco.com>"
+__contributors__ = [
+]
+__copyright__ = "Copyright (c) 2018 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.0"
+
 
 import requests
 import json
--- nxapi_cli/step-03/payloads.py
+++ nxapi_cli/step-03/payloads.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
-"""
-Given a set of CLI commands, generate a payload based on the message and command types.
+# -*- coding: utf-8 -*-
+""" 
+Copyright (c) 2018 Cisco and/or its affiliates.
 
-Current supported message types are:  json-rpc (2.0)
-Current supported command types are: 
-    (json-rpc) cli/cli_ascii and 
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.0 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
 
 """
 
 # http://python-future.org/quickstart.html
 from __future__ import absolute_import, division, print_function
+
+__author__ = "Timothy E Miller, PhD <timmil@cisco.com>"
+__contributors__ = [
+]
+__copyright__ = "Copyright (c) 2018 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.0"
 
 import copy
 
